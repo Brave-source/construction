eaa877d534d9535aaf614cd81c3761f875029d0a
timothy-eldon <149790424+timothy-eldon@users.noreply.github.com>
2024-05-30 13:32:23 +0100

    create nav

---
+++ resources/views/components/navbar.blade.php
@@ -0,0 +1,17 @@
+<div>
+    <nav class="absolute top-0 left-0 w-full flex items-center justify-between p-6 bg-transparent">
+        <div class="flex items-center text-white text-lg font-bold">
+            <img src="../logo.png" alt="Logo" class="h-10 mr-3">
+            <span>Ignition633</span>
+        </div>
+        <ul class="flex space-x-6 text-white">
+            <li><a href="#home" class="hover:text-gray-300">Home</a></li>
+            <li><a href="#about" class="hover:text-gray-300">About</a></li>
+            <li><a href="#services" class="hover:text-gray-300">Services</a></li>
+            <li><a href="#Project" class="hover:text-gray-300">Project</a></li>
+            <li><a href="#Blog" class="hover:text-gray-300">Blog</a></li>
+            <li><a href="#contact" class="hover:text-gray-300">Contact</a></li>
+            <li><a href="#Pages" class="hover:text-gray-300">Pages</a></li>
+        </ul>
+    </nav>
+</div>
